8b3d35ab5390556b569e02697fbdfa4bf5e5b672
thisguyagain-39 <cframpto@confederationcollege.ca>
2025-02-04 14:48:34 -0500

    early push; why do my internal links Not Work

--- _site/js/main.js
+++ _site/js/main.js
@@ -3,6 +3,9 @@ var contentainter = document.querySelector(".internalContent")
 console.log(contentainter)
 // fetch json from external source here... we'll use a local file for now
 
+var thisPage = document.title;
+
+console.log(thisPage)
 fetchContent()
 
 async function fetchContent() {
@@ -31,77 +34,7 @@ async function fetchContent() {
 
         data = await response.json()
 
-        data.forEach(package => {
-
-            // format stuff
-
-            var thisTitle = package.title;
-
-            var thisContent = package.text;
-
-            var thisAssets = undefined;
-            
-            let assetsArray = []
-
-            console.log(package.assets)
-
-            if (package.assets !== (undefined)) { // if assets are not undefined
-
-                thisAssets = package.assets;
-
-                    {thisAssets.forEach(asset => {
-
-                        if (getExtension(asset) == ".png" || ".jpg" || ".jpeg" || ".webp" || ".gif" || ".svg" || ".heif" || ".tiff"){ // check for image file types 
-
-                            //if its an image, put it in an image element
-
-                            var anAsset = document.createElement("img")
-
-                            anAsset.src = asset
-
-                            assetsArray.push(anAsset)
-
-                        } else if (getExtension(asset) == ".mp4" || ".ogg" || ".webm" ) { // check for video file types
-
-                            // if it's a video, make it a video
-
-                            var anAsset = document.createElement("video")
-
-                            anAsset.src = asset
-
-                            assetsArray.push(anAsset)
-
-                        }
-
-                        console.log(asset)
-
-                        // todo: mkv support so I dont have to fuss with conversion straight from my computer, audio support
-
-                    }
-                        
-                )
-            }
-
-            }
-
-            console.log("on package " + thisTitle, thisContent, thisAssets, assetsArray)
-
-            var toInsertThis = document.createElement("div")
-
-            toInsertThis.innerHTML = `<h2> ${thisTitle} </h2> <p> ${thisContent} </p>`
-
-            assetsArray.forEach(asset => {
-
-                toInsertThis.appendChild(asset)
-                
-            });
-
-            contentainter.appendChild(toInsertThis)
-
-
-        }
-
-    )
+        mainFormatter(data)
 
     } catch(error) {
 
@@ -111,7 +44,9 @@ async function fetchContent() {
 
 }
 
-// yeah i nabbed this from stackoverflow....
+// yeah i nabbed this from stackoverflow.... thank yoooouuu stackoverflow
+
+// source: https://stackoverflow.com/a/12900504
 
 function getExtension(path) {
 
@@ -125,3 +60,80 @@ function getExtension(path) {
 
     return basename.slice(pos + 1);            // extract extension ignoring `.`
 }
+
+function mainFormatter(data) {
+
+    data.forEach(package => {
+
+        // format stuff
+
+        var thisTitle = package.title;
+
+        var thisContent = package.text;
+
+        var thisAssets = undefined;
+        
+        let assetsArray = []
+
+        console.log(package.assets)
+
+        if (package.assets !== (undefined)) { // if assets are not undefined
+
+            thisAssets = package.assets;
+
+                {thisAssets.forEach(asset => {
+
+                    if (getExtension(asset) == ".png" || ".jpg" || ".jpeg" || ".webp" || ".gif" || ".svg" || ".heif" || ".tiff"){ // check for image file types 
+
+                        //if its an image, put it in an image element
+
+                        var anAsset = document.createElement("img")
+
+                        anAsset.src = asset
+
+                        assetsArray.push(anAsset)
+
+                    } else if (getExtension(asset) == ".mp4" || ".ogg" || ".webm" ) { // check for video file types
+
+                        // if it's a video, make it a video
+
+                        var anAsset = document.createElement("video")
+
+                        anAsset.src = asset
+
+                        assetsArray.push(anAsset)
+
+                    }
+
+                    console.log(asset)
+
+                    // todo: mkv support so I dont have to fuss with conversion straight from my computer, audio support
+
+                }
+                    
+            )
+        }
+
+        }
+
+        console.log("on package " + thisTitle, thisContent, thisAssets, assetsArray)
+
+        var toInsertThis = document.createElement("div")
+
+        toInsertThis.innerHTML = `<h2> ${thisTitle} </h2> <p> ${thisContent} </p>`
+
+        assetsArray.forEach(asset => {
+
+            toInsertThis.appendChild(asset)
+            
+        });
+
+        contentainter.appendChild(toInsertThis)
+
+
+        }
+
+    )
+}
+
+
